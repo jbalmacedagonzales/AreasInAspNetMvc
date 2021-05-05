721d81cf8b989e51fa2e4e5015ec4e3c2dfba849
jbalmacedagonzales <jbalmacedagonzales@JOSE>
2021-05-04 23:59:23 -0500

    Agregar archivos de proyecto.

---
+++ AreasInAspNetMvc/Areas/Clients/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AreasInAspNetMvc.Areas.Clients.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Clients/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AreasInAspNetMvc/Areas/Employees/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AreasInAspNetMvc.Areas.Employees.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Employees/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
